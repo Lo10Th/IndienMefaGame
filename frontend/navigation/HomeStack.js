0c6c5d24da7be3a7db232281f5cb88c855312c55
Lo10Th <karl.cornelius100@gmail.com>
2024-04-29 20:29:24 +0200

    Added base structure for the mobile app

---
+++ frontend/navigation/HomeStack.js
@@ -0,0 +1,14 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+const HomeStack = createStackNavigator();
+
+export default function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
---
+++ frontend/navigation/SettingsStack.js
@@ -0,0 +1,12 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const SettingsStack = createStackNavigator();
+
+export default function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
